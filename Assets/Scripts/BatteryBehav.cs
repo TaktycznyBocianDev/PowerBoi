b33c6529fcefdc2d4ab5e44c4a754e5e822d7fbf
Stanisław Franczak <taktycznybocian@gmail.com>
2023-07-16 23:01:01 +0200

    Stations

--- Assets/Scripts/BatteryBehav.cs
+++ Assets/Scripts/BatteryBehav.cs
@@ -7,6 +7,7 @@ public class BatteryBehav : MonoBehaviour
 {
     public float maxEnergyLevel;
     public float currentEnergyLevel;
+    public float dropRate;
     public PowerOnBatteryLevel powerOnBatteryLevel;
 
     Rigidbody rb;
@@ -64,6 +65,7 @@ public class BatteryBehav : MonoBehaviour
 
     void BatteryOnFloor()
     {
+        StopAllCoroutines();
         SetBatteryParent(null);
         rb.isKinematic = false;
         rb.useGravity = true;
@@ -74,7 +76,7 @@ public class BatteryBehav : MonoBehaviour
 
     void BatteryInHand()
     {
-        
+        StopAllCoroutines();
         rb.isKinematic = true;      
         rb.useGravity = false;
         transform.rotation = Quaternion.Euler(0, -45, 0);
@@ -87,6 +89,7 @@ public class BatteryBehav : MonoBehaviour
         rb.useGravity = false;
         transform.rotation = Quaternion.Euler(0, 0, 0);
         transform.localScale = new Vector3(0.8f, 0.4f, 0.8f);
+        StartCoroutine(DropEnergy());
     }
 
     public void SetBatteryParent(Transform newParent)
@@ -97,5 +100,23 @@ public class BatteryBehav : MonoBehaviour
     public void SetBatteryPosition(Vector3 position)
     {
         transform.position = position;
+    }
+
+    public IEnumerator DropEnergy()
+    {
+        while (currentEnergyLevel > 0 )
+        {
+            currentEnergyLevel--;
+            powerOnBatteryLevel.SetEnergyOnDisplay(currentEnergyLevel);
+            yield return new WaitForSeconds(dropRate);
+        }
+
+        if (currentEnergyLevel < 0)
+        {
+            Debug.Log("No more energy in this battery!");
+            currentEnergyLevel = 0;
+        }
+
+        
     }
 }
